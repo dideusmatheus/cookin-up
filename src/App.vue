0580b0f90c9053f93b280044ee8c3a19d3fff09d
Matheus de Deus Rocha <matheus.rocha@sis-it.com>
2024-07-10 15:35:22 -0300

    Criando o componente MainContent e chamando ele no app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <BannerTop></BannerTop>
+  <MainContent></MainContent>
 </template>
 
 <script>
   import BannerTop from '@/components/BannerTop.vue'
+  import MainContent from './components/MainContent.vue'
   export default {
     components: {
-      BannerTop
+      BannerTop,
+      MainContent
     }
   }
 </script>
